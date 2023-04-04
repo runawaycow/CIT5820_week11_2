92f4ebe1980a229bccfda4100686a8b82c8589d1
Dan Luo <luo_dana@yahoo.com>
2023-04-05 06:43:12 +1000

    Update send_tokens.py

--- send_tokens.py
+++ send_tokens.py
@@ -43,7 +43,7 @@ def send_tokens( receiver_pk, tx_amount ):
     print(signed_txn )
     # Send the signed transaction to the blockchain
     txid = 'sss'
-    txid = algod_client.send_transaction(signed_txn)
+    txid = AlgodClient.send_transaction(signed_txn)
     #txid = 'sss'
     return sender_address , txid
 
