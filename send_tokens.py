6308275e96eda2edc1d5a643b56aa31877cf9739
Dan Luo <luo_dana@yahoo.com>
2023-04-05 06:37:13 +1000

    Update send_tokens.py

--- send_tokens.py
+++ send_tokens.py
@@ -39,10 +39,11 @@ def send_tokens( receiver_pk, tx_amount ):
         amt=tx_amount,  # Amount in microalgos (1 ALGO = 1,000,000 microalgos)
     )
     # Sign the transaction with the sender's private key
-    signed_txn = payment_txn.sign(sender_private_key)
+    #signed_txn = payment_txn.sign(sender_private_key)
 
     # Send the signed transaction to the blockchain
-    txid = algod_client.send_transaction(signed_txn)
+    #txid = algod_client.send_transaction(signed_txn)
+    txid = 'sss'
     return sender_address , txid
 
 
